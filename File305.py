c843bb06f9313f0486592572f1c0e27ef9aa1d79
AmenaFarha <amena15-11845@diu.edu.bd>
2024-06-03 18:04:31 -0400

    account2

---
+++ File305.py
@@ -0,0 +1,6 @@
+# This is file number 305
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(305, 610)
+print(f'Result of adding 305 and 610 is:', result)
---
+++ File372.py
@@ -0,0 +1,6 @@
+# This is file number 372
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(372, 744)
+print(f'Result of adding 372 and 744 is:', result)
