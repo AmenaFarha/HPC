104085d59b6a7544f803ae5b77739a7a9f4e3fee
AmenaFarha <amena15-11845@diu.edu.bd>
2024-06-03 17:52:58 -0400

    account3

---
+++ File303.py
@@ -0,0 +1,6 @@
+# This is file number 303
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(303, 606)
+print(f'Result of adding 303 and 606 is:', result)
---
+++ File320.py
@@ -0,0 +1,6 @@
+# This is file number 320
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(320, 640)
+print(f'Result of adding 320 and 640 is:', result)
