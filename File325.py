eb8e59605c94a4714a9617e1918c9f8a814f1007
AmenaFarha <amena15-11845@diu.edu.bd>
2024-06-03 18:04:32 -0400

    account2

---
+++ File325.py
@@ -0,0 +1,6 @@
+# This is file number 325
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(325, 650)
+print(f'Result of adding 325 and 650 is:', result)
---
+++ File355.py
@@ -0,0 +1,6 @@
+# This is file number 355
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(355, 710)
+print(f'Result of adding 355 and 710 is:', result)
